f8bf5407f8f33f8c429358afa71da5bc50676ee9
4got10king <Aldavletshina77654@yandex.ru>
2023-04-22 00:00:00 +0500

    CryptoConv

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+import  requests
+from config import keys, TOKEN
+from utils import CovertionException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'To get started, enter the bot command in the following format: \n <value start> \ <value end> \ <many> \nget available values: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message:telebot.types.Message):
+    text = 'Values:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise CovertionException('Wrong parameters')
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except CovertionException as e:
+        bot.reply_to(message, f'User error: {e}')
+    except Exception as e:
+        bot.reply_to(message, f'Error : {e}')
+    else:
+        text = f'Price {amount} {quote} in {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
