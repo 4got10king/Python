6eb1be57fd37eb1822e2ac2c545af913ea85fda7
Adel <Aldavletshina777654@yandex.ru>
2024-03-01 20:05:01 +0300

    test

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/items/{item_id}')
+@app.get('/{item_id}')
 async def read_item(item_id):
-	return{'message': item_id }
+	if item_id == 'Aleksander':
+		return 2+2
+	elif item_id == 'Maksim':
+		return 'AK47'
+	else:
+		return{'message': item_id }
