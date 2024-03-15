f4e166bab801358e2247cedc94db6ba784f395b6
Adel <Aldavletshina777654@yandex.ru>
2024-03-15 22:29:01 +0300

    Handler booklist init

--- telebot/BookMarketTG/bookmarkettg/bot.py
+++ telebot/BookMarketTG/bookmarkettg/bot.py
@@ -23,11 +23,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text=START
     )
 
+async def booklist(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text = 'Здесь скоро появится список книг'
+
+    )
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     
     start_handler = CommandHandler('start', start)
+    booklist_handler = CommandHandler('booklist', booklist)
     application.add_handler(start_handler)
+    application.add_handler(booklist_handler)
     
     application.run_polling()
 
